5e6713eeac74a8cdf1905071203276815c716940
jisu <shinjs0505@naver.com>
2021-07-15 10:54:00 +0900

    Rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import HomeScreen from './Screens/HomeScreen';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>JisuShoppingMall</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import products from '../Products'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../Components/Product';
+
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Lastest Products</h1>
+            <Row>
+                {products.map(item => (
+                    <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={item} />
+                    </Col>
+                ))}
+            </Row>
+            
+        </>
+    );
+};
+
+export default HomeScreen;
