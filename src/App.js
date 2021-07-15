2d48d58a6e0c68855a74ab7d10202d4962a78b2e
jisu <shinjs0505@naver.com>
2021-07-15 10:33:58 +0900

    Bootstrap, fontawesome적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 const App = () => {
   return (
-    <div>
-      <h1>Shopping!</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>JisuShoppingMall</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
